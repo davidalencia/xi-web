79a1ad85c96aee229461a81a6fed521b44f83bdc
Valencia <davidalencia@gmail.com>
2020-02-02 14:23:03 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
+}
